caf581e2d3a1d24218c178f2dbb9f20aa402d696
nbonnie <nrbmee@gmail.com>
2024-06-29 20:55:55 -0400

    clarifying use via filenames, creating a standard max processing fs

--- create_MAX_IR_fs.sh
+++ create_MAX_IR_fs.sh
@@ -11,7 +11,7 @@
 #
 # Author:      Nolan R. Bonnie
 # Contact:     nolan.bonnie@colorado.edu 
-# Created:     06/2024
+# Created:     04/2024
 #--------------------------------------------------------------------------------
 
 # Check if an input directory is provided
@@ -23,7 +23,7 @@ fi
 parent_dir="$1"
 
 # Directories to create
-sub_dirs=("MAX_360" "MAX_mp4" "MAX_xyt" "MAX_xyzt")
+sub_dirs=("MAX_IR_Raw_360" "MAX_IR_mp4_Chapters" "MAX_IR_mp4_Sphere" "MAX_IR_xyt" "MAX_IR_xyzt")
 cam_dirs=("cam1" "cam2")
 
 # Create the parent directory if it doesn't exist
@@ -35,7 +35,7 @@ for dir in "${sub_dirs[@]}"; do
   mkdir -p "$full_path"  # Create the subdirectory if it doesn't exist
 
   # Create 'cam1' and 'cam2' directories inside specified subdirectories
-  if [[ "$dir" == "MAX_360" || "$dir" == "MAX_mp4" || "$dir" == "MAX_xyt" ]]; then
+  if [[ "$dir" == "MAX_IR_Raw_360" || "$dir" == "MAX_IR_mp4_Chapters" || "$dir" == "MAX_IR_mp4_Sphere" || "$dir" == "MAX_IR_xyt" ]]; then
     for cam in "${cam_dirs[@]}"; do
       cam_path="$full_path/$cam"
       mkdir -p "$cam_path"
